c024cdb83f8920ea5627c05efd3263ac46fe940a
tykieph <tykieph@gmail.com>
2018-03-29 00:17:13 +0200

    tables in loops v2

--- projekt/application/views/pages/equipment.php
+++ projekt/application/views/pages/equipment.php
@@ -17,7 +17,7 @@
                 "Cechy drugorzędne", "Początkowa", "Schemat rozwoju", "Aktualna"
             );
 
-            /*      cells values    */
+            /*      cells' values    */
             $cols = array
             (
                 0 => array("WW", "US", "K", "ODP", "ZR", "INT", "SW", "OGD"),
@@ -47,9 +47,9 @@
                 {
                     if($i == 0)
                     /*      1st row(no border)      */
-                        echo "<div class=col-1 text-center>".$cols[$i][$j]."</div>";
+                        echo "<div class='col-1 text-center'>".$cols[$i][$j]."</div>";
                     else
-                        echo "<div class=col-1 text-center id='cell'>".$cols[$i][$j]."</div>";
+                        echo "<div class='col-1 text-center' id='cell'>".$cols[$i][$j]."</div>";
                 }
 
                 ?>
@@ -68,37 +68,55 @@
     <div class="col-8">
         <ul class="list-group" id="character">
             <li class="list-group-item info_heading"><h3>BROŃ</h3></li>
+            <?php
 
-            <li class="list-group-item">
-                <div class="row">
-                    <div class="col-4">Nazwa: </div>
-                    <div class="col-1 text-center">OBC.</div>
-                    <div class="col-1 text-center">KAT.</div>
-                    <div class="col-1 text-center">SIŁA</div>
-                    <div class="col-1 text-center">ZASIĘG</div>
-                    <div class="col-1 text-center">PRZEŁAD.</div>
-                    <div class="col-3 text-center">CECHY ORĘŻA</div>
-                </div>
-            </li>
+            /*      number of rows and colums       */
+            $num_rows = 6;
+            $num_cols = 7;
 
-<?php
-    for($i=0; $i<=4; $i++)
-    {
-?>
-            <li class="list-group-item">
-                <div class="row">
-                    <div class="col-4" id="cell">&nbsp</div>
-                    <div class="col-1" id="cell"></div>
-                    <div class="col-1" id="cell"></div>
-                    <div class="col-1" id="cell"></div>
-                    <div class="col-1" id="cell"></div>
-                    <div class="col-1" id="cell"></div>
-                    <div class="col-3" id="cell"></div>
-                </div>
-            </li>
-<?php 
-    };
-?>
+            /*      cells' values    */
+            $cols = array
+            (
+                0 => array("Nazwa", "OBC.", "KAT.", "SIŁA", "ZASIĘG", "PRZEŁAD", "CECHY ORĘŻA"),
+                1 => array_fill(0, 8, "&nbsp"),
+                2 => array_fill(0, 8, "&nbsp"),
+                3 => array_fill(0, 8, "&nbsp"),
+                4 => array_fill(0, 8, "&nbsp"),
+                5 => array_fill(0, 8, "&nbsp"),
+            );
+
+            for($i = 0; $i < $num_rows; $i++)
+            {
+                ?>
+                <li class="list-group-item">
+                    <div class="row">
+                <?php
+                for($j = 0; $j < $num_cols; $j++)
+                {
+                    /*       first row(header)       */
+                    if($i == 0)
+                    {
+                        if($j == 0)
+                            echo "<div class='col-4'>";
+                        else if($j == 6)
+                            echo "<div class='col-3 text-center'>";
+                        else
+                            echo "<div class='col-1 text-center'>";
+                    }
+                    else
+                    {
+                        if($j == 0)
+                            echo "<div class='col-4' id='cell'>";
+                        else if($j == 6)
+                            echo "<div class='col-3' id='cell'>";
+                        else
+                            echo "<div class='col-1 text-center' id='cell'>";
+                    }
+
+                    echo $cols[$i][$j]."</div>";
+                }
+            }
+            ?>
         </ul>
     </div>
     <div class="col-2"></div>
