ad54dde4be90ad661cc6a8e3f59a84544aa277f1
tykieph <tykieph@gmail.com>
2018-03-30 16:22:16 +0200

    player controller

--- projekt/application/controllers/Player.php
+++ projekt/application/controllers/Player.php
@@ -47,10 +47,6 @@ class Player extends CI_Controller
 	public function update()
     {
         $this->player_model->update();
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/character_pagination', $data);
-        $this->load->view('pages/player/equipment', $data);
-        $this->load->view('templates/footer');
     }
 
 }
