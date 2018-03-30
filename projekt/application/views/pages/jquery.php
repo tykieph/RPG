2b9925b45a74cca61badd95889561e27435f0c4a
Delyew <delyew@gmail.com>
2018-03-30 10:30:59 +0200

    uwaga czy dziala

---
+++ projekt/application/views/pages/jquery.php
@@ -0,0 +1,7 @@
+<?=validation_errors()?>
+<?=form_open('jquery/create')?>
+<label for="value">Value</label>
+<input type="number" name="value">
+<script>
+
+</script>
--- projekt/application/views/templates/character_pagination.php
+++ projekt/application/views/templates/character_pagination.php
@@ -5,5 +5,6 @@
         </li>
         <li class="page-item"><a class="page-link" href="equipment">Wyposażenie</a></li>
         <li class="page-item"><a class="page-link" href="skills">Zdolności</a></li>
+        <li class="page-item"><a class="page-link" href="jquery">Zabaw sie jquery</a></li>
     </ul>
 </nav>
